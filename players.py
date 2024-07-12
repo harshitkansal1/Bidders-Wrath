4f5ed4c6dd3cf6f6acac7d1e5360faf5252250b1
harshitkansal1 <164679232+harshitkansal1@users.noreply.github.com>
2024-07-12 16:22:56 +0530

    Update players.py

--- players.py
+++ players.py
@@ -69,4 +69,70 @@ class AdaptivePlayer:
 
     def round_result(self, my_chance, bids, results):
         self.score += results[my_chance-1]
+class HarshitKansal:
+    def __init__(self):
+        self.name = 'Harshit Kansal'
+        self.score = 0
+
+    def bid(self, purse, current_bids, totalscores):
+        # Identify player's position on the leaderboard
+        my_position = sorted(range(len(totalscores)), key=lambda k: totalscores[k], reverse=True).index(self.score)
+
+        # Case 1: First bid chance
+        if len(current_bids) == 0:
+            if my_position == 0:
+                return 16
+            elif my_position == 1:
+                return 18
+            else:
+                return 21
         
+        # Case 2: Second bid chance
+        elif len(current_bids) == 1:
+            first_bidder_position = sorted(range(len(totalscores)), key=lambda k: totalscores[k], reverse=True).index(totalscores[0])
+            first_bid = current_bids[0]
+            if first_bidder_position == 0 and first_bid > 16:
+                return first_bid - 1
+            elif first_bid > 20:
+                return first_bid - 1
+            else:
+                return purse // 2 + 1 if first_bidder_position != 0 else first_bid // 2 + 4
+        
+        # Case 3: Third bid chance
+        elif len(current_bids) == 2:
+            declines = current_bids.count(-1)
+            second_bidder_position = sorted(range(len(totalscores)), key=lambda k: totalscores[k], reverse=True).index(totalscores[1])
+            second_bid = current_bids[1]
+            if declines == 2:
+                return -1
+            elif declines == 1:
+                if second_bidder_position == 0 and second_bid > 18:
+                    return second_bid - 1
+                else:
+                    return purse // 2 + 1 if second_bidder_position != 0 else purse // 2 + 4
+            else:
+                if second_bid > 22:
+                    return second_bid - 1
+                else:
+                    return purse // 2 + 1 if second_bidder_position != 0 else purse // 2 + 4
+        
+        # Case 4: Fourth bid chance
+        elif len(current_bids) == 3:
+            declines = current_bids.count(-1)
+            third_bidder_position = sorted(range(len(totalscores)), key=lambda k: totalscores[k], reverse=True).index(totalscores[2])
+            third_bid = current_bids[2]
+            if declines == 3 or (declines == 2 and third_bid == 0):
+                return -1
+            elif declines == 1:
+                if third_bidder_position == 0:
+                    return -1
+                else:
+                    return purse if purse > 14 else -1
+            else:
+                return purse
+
+    def round_result(self, my_chance, bids, results):
+        self.score += results[my_chance-1]
+
+# Adding HarshitKansal player to the game
+
